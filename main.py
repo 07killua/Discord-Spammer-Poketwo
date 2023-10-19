8ece2fa868dfc2ce6a8cd00824ab7f5ec7cefa3b
07killua <148428996+07killua@users.noreply.github.com>
2023-10-19 12:55:54 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from webserver import keep_alive
 import requests
 
-channelID = PUT THE CHANNEL ID
+channelID = 1017792645562630304
 headers = {
-    "authorization":
+    "authorization": OTU0NDExOTg1NjQ1MjExNjQ5.GoSaLf.OB_R5q39UCwMCA8__w1_C-QYILpDdbXl_Qs6qE
     "YOUR TOKEN HERE"
 }
 keep_alive()
