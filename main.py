1337eaa347a3bc2e0164e4221dd89ce363f44071
07killua <148428996+07killua@users.noreply.github.com>
2023-10-19 12:59:41 +0600

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import requests
 
 channelID = 1017792645562630304
 headers = {
-    "authorization": OTU0NDExOTg1NjQ1MjExNjQ5.GoSaLf.OB_R5q39UCwMCA8__w1_C-QYILpDdbXl_Qs6qE
-    "YOUR TOKEN HERE"
+    "authorization": 
+    "OTU0NDExOTg1NjQ1MjExNjQ5.GoSaLf.OB_R5q39UCwMCA8__w1_C-QYILpDdbXl_Qs6qE"
 }
 keep_alive()
 file = open("text.txt", "r")
